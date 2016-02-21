9194065b2db682be92496ea2c295cf2bbfa6fd8d
pulkitbhutani <pulkitbhutani@outlook.com>
2016-02-22 01:57:46 +0530

    Make a basic User model

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,6 +9,7 @@ test "layout links" do
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
 end
   # test "the truth" do
   #   assert true
