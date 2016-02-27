464167d8fddb9b6b59ee0b552850a6e163866ceb
pulkitbhutani <pulkitbhutani@outlook.com>
2016-02-28 04:08:11 +0530

    Finished User Signupchanges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    
   end
 
   def create
@@ -21,7 +22,8 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 	  validates :email, presence: true, length:{maximum: 255},
 	  format: { with: VALID_EMAIL_REGEX },uniqueness: true
 
-	  has_secure_password
+	  
 	  validates :password, presence: true, length: { minimum: 6 }
-
+	  has_secure_password
 end
